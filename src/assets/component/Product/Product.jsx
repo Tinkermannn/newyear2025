d4e11a418101c90f064ff5c9e2d9b238da3b1b40
Tinkermannn <wilmansaragih2@gmail.com>
2025-01-28 14:18:10 +0700

    Testing cors from backend

--- src/assets/component/Product/Product.jsx
+++ src/assets/component/Product/Product.jsx
@@ -10,7 +10,7 @@ export default function Product() {
 
     const fetchPosts = async () => {
         try {
-            const response = await axios.get("http://localhost:8000/user/posts/");
+            const response = await axios.get("https://renter-be.vercel.app/user/posts");
             setPosts(response.data.posts);
         } catch (err) {
             setError("Gagal mengambil data dari server");
